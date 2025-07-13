b4541e99305da0328572276ebacc3f030aa0cde3
sakshehehe <sakshipawale03@gmail.com>
2025-07-13 11:52:13 +0530

    env changes

--- server.js
+++ server.js
@@ -16,10 +16,8 @@ app.use(cors({
 }));
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
+
 .then(() => console.log('MongoDB Connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
